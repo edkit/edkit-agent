4a65c18ed07d244b6e394c8ffa2190634aa779b6
MainRo <romain.picard@oakbits.com>
2013-05-28 22:44:03 +0200

    add support for sub-components in makefile

--- build/srcbuild.mk
+++ build/srcbuild.mk
@@ -2,16 +2,39 @@ C_OBJS = $(SRCS:.c=.o)
 CXX_OBJS = $(CXX_SRCS:.cpp=.o)
 OBJS = $(C_OBJS) $(CXX_OBJS)
 
+BUILD_TYPE_EXE=exe
+BUILD_TYPE_STATIC_LIB=staticlib
+BUILD_TYPE_DYN_LIB=dynlib
+# exe
+EXE_BUILD = $(findstring $(BUILD_TYPE_EXE),$(BUILD_TYPE))
+ifeq ($(EXE_BUILD),$(BUILD_TYPE_EXE))
 TARGET_NAME = $(NAME)
-ifeq ($(BUILD_TYPE),staticlib)
-TARGET_NAME = lib$(NAME).a
-else
-ifeq ($(BUILD_TYPE),dynlib)
-TARGET_NAME = lib$(NAME).so
-endif
+OUT=$(BUILD_TYPE_EXE)
 endif
 
-all: $(BUILD_TYPE)
+# static lib
+STATIC_BUILD = $(findstring $(BUILD_TYPE_STATIC_LIB),$(BUILD_TYPE))
+ifeq ($(STATIC_BUILD),$(BUILD_TYPE_STATIC_LIB))
+TARGET_NAME = lib$(NAME).a
+OUT=$(BUILD_TYPE_STATIC_LIB)
+endif
+
+# dynamic lib
+DYN_BUILD = $(findstring $(BUILD_TYPE_DYN_LIB),$(BUILD_TYPE))
+ifeq ($(DYN_BUILD),$(BUILD_TYPE_DYN_LIB))
+TARGET_NAME = lib$(NAME).so
+OUT=$(BUILD_TYPE_DYN_LIB)
+endif
+
+# sub-components
+SUB_COMPONENTS_RULES:=$(addsuffix .subcomponent,$(SUB_COMPONENTS))
+SUB_COMPONENTS_RULES_CLEAN:=$(addsuffix .subcomponent_clean,$(SUB_COMPONENTS))
+
+# common compiler options
+CPPFLAGS+=-I$(CURDIR)/src -I$(CURDIR)/include
+
+# common targets
+all: $(SUB_COMPONENTS_RULES) $(OUT)
 
 exe: $(OBJS)
 	$(CXX) -o $(NAME) $(OBJS) $(LDFLAGS)
@@ -22,7 +45,7 @@ staticlib: $(OBJS)
 dynlib: $(OBJS)
 	$(CXX) -shared -Wl,-soname,$(TARGET_NAME) -o $(TARGET_NAME) $(OBJS) $(LDFLAGS)
 
-clean:
+clean: $(SUB_COMPONENTS_RULES_CLEAN)
 	rm -f $(C_OBJS)
 	rm -f $(CXX_OBJS)
 	rm -f $(TARGET_NAME)
@@ -32,3 +55,10 @@ clean:
 
 %.o : %.cpp
 	$(CXX) $(CPPFLAGS) $(CFLAGS) $(CXXFLAGS) -o $@ -c $<
+
+%.subcomponent:
+	make -f Component.mk -C $(CURDIR)/$(basename $@)
+
+%.subcomponent_clean:
+	make -f Component.mk -C $(CURDIR)/$(basename $@) clean
+
