bbe933ffeeede14076bafeb08d151d7a39cbc9b4
MainRo <romain.picard@oakbits.com>
2013-11-24 23:12:53 +0100

    Use leak factor in timePlot and add allocer parameter in sizePlot

--- client/python/edleak/plot.py
+++ client/python/edleak/plot.py
@@ -10,15 +10,14 @@ class SlicePlotter(object):
       self.asset = asset
       self.slice_batch = asset.getSliceList()
 
-   def sizePlot(self, scale='linear'):
+   def sizePlot(self, scale='log', allocers=None):
       """
       Plots an edleak slice batch as a scatter plot with the size on the x axis.
       This is the default and recommended plot type for visual analysis.
       """
-      allocers = self.asset.getAllocerList()
+      if allocers == None:
+         allocers = self.asset.getAllocerList()
 
-      slice_count = len(allocers[0]['coring'])
-      allocer_count = len(allocers)
       colormaps = {}
       colormaps[leak_factor.leaker_class_constant] = plt.get_cmap('Greens')
       colormaps[leak_factor.leaker_class_linear] = plt.get_cmap('Reds')
@@ -47,34 +46,39 @@ class SlicePlotter(object):
          ax.set_xscale('log')
       plt.show()
 
-   def timePlot(self, scale='linear', allocers=None):
+   def timePlot(self, scale='log', allocers=None):
       """
       Plots an edleak slice batch as a scatter plot with the time on the x axis.
       """
-      slice_count = len(self.slice_batch)
-      context_count = len(self.slice_batch[slice_count-1])
+      if allocers == None:
+         allocers = self.asset.getAllocerList()
 
-      x = np.r_[0:slice_count]
-      y = np.zeros((context_count, slice_count), int)
-
-      greens = plt.get_cmap('Greens')
-      cnorm  = colors.Normalize(vmin=0, vmax=context_count)
-      scalar_map = cm.ScalarMappable(norm=cnorm, cmap=greens)
+      cnorm  = colors.Normalize(vmin=0, vmax=len(allocers))
+      colormaps = {}
+      colormaps[leak_factor.leaker_class_constant] = cm.ScalarMappable(norm=cnorm, cmap=plt.get_cmap('Greens'))
+      colormaps[leak_factor.leaker_class_linear] = cm.ScalarMappable(norm=cnorm, cmap=plt.get_cmap('Reds'))
+      colormaps[leak_factor.leaker_class_log] = cm.ScalarMappable(norm=cnorm, cmap=plt.get_cmap('Blues'))
+      colormaps[leak_factor.leaker_class_exp] = cm.ScalarMappable(norm=cnorm, cmap=plt.get_cmap('binary'))
 
       ax = plt.subplot(111)
-      slice_index = 0
-      for current_slice in self.slice_batch:
-         for alloc_point in current_slice:
-            y[alloc_point['alc']][slice_index] = alloc_point['mem']
-         slice_index += 1
 
-      context_index = 0
-      while context_index < context_count:
-         if allocers == None or context_index in allocers:
-            color_value = scalar_map.to_rgba(context_index)
-            ax.scatter(x, y[context_index], c=color_value, s=60, edgecolors='none')
-         context_index += 1
+      allocer_index = 0
+      for allocer in allocers:
+         coring = allocer['coring']
+         x = np.r_[0:len(coring)]
+         y = np.empty(len(coring))
+
+
+         slice_index = 0
+         for alloc_point in coring:
+            y[slice_index] = alloc_point
+            slice_index += 1
+
+         color_value = colormaps[allocer['leak_factor']['class']].to_rgba(allocer_index)
+         ax.scatter(x, y, c=color_value, s=60, edgecolors='none')
+         allocer_index += 1
 
       if scale == 'log':
          ax.set_yscale('log')
       plt.show()
+
